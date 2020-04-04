626eb24b453c2660984e25614c8e06fdce3b0c4e
RedGhoul <avaneesab5@gmail.com>
2020-04-04 19:00:13 -0400

    meh

--- JobtransparencyNLP/__init__.py
+++ JobtransparencyNLP/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-#from config import SECRET_KEY,SQLALCHEMY_DATABASE_URI
+# from config import SECRET_KEY,SQLALCHEMY_DATABASE_URI
 login_manager = LoginManager()
 
 app = Flask(__name__)
