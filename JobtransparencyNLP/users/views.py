56822ca6d9da6d9b126b4e8ee9a7dd5ca1f2002a
RedGhoul <avaneesab5@gmail.com>
2020-04-04 19:33:00 -0400

    fixed login

--- JobtransparencyNLP/users/views.py
+++ JobtransparencyNLP/users/views.py
@@ -19,7 +19,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
 
-        if user.check_password(form.password.data) and user is not None:
+        if user is not None and user.check_password(form.password.data):
             login_user(user)
             flash('Logged in')
             next = request.args.get('next')
