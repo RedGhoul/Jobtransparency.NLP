fd921911fa5fbf35bbac5f9edd66e934cc9dbd48
RedGhoul <avaneesab5@gmail.com>
2020-04-05 12:12:55 -0400

    added if summary is null clause

--- JobtransparencyNLP/NLTKProcessor.py
+++ JobtransparencyNLP/NLTKProcessor.py
@@ -43,7 +43,7 @@ def _score_sentences(sentences, freqTable) -> dict:
                 else:
                     sentenceValue[sentence[:10]] = freqTable[wordValue]
 
-        sentenceValue[sentence[:10]] = sentenceValue[sentence[:10]] // word_count_in_sentence
+        sentenceValue[sentence[:10]] = sentenceValue[sentence[:10]] #word_count_in_sentence
 
     return sentenceValue
 
@@ -63,27 +63,35 @@ def _generate_summary(sentences, sentenceValue, threshold):
     summary = ''
 
     for sentence in sentences:
-        if sentence[:10] in sentenceValue and sentenceValue[sentence[:10]] > (threshold):
+        if sentence_count > 3:
+            break
+        if sentence[:10] in sentenceValue and sentenceValue[sentence[:10]] >= (threshold):
             summary += " " + sentence
             sentence_count += 1
 
     return summary
 # https://becominghuman.ai/text-summarization-in-5-steps-using-nltk-65b21e352b65
-def generate_summary(textIn, top_n=5):
+def generate_summary(textIn):
     soup = BeautifulSoup(textIn)
     textIn = soup.get_text()
     textIn = textIn.replace("-","")
     textIn = textIn.strip()
     # 1 Create the word frequency table
     freq_table = _create_frequency_table(textIn)
+    #print(freq_table)
     # 2 Tokenize the sentences
     sentences = sent_tokenize(textIn)
+    #print(sentences)
     # 3 Important Algorithm: score the sentences
     sentence_scores = _score_sentences(sentences, freq_table)
+    #print(sentence_scores)
     # 4 Find the threshold
     threshold = _find_average_score(sentence_scores)
+    #print(threshold)
     # 5 Important Algorithm: Generate the summary
-    summary = _generate_summary(sentences, sentence_scores, 1.5 * threshold)
+    summary = _generate_summary(sentences, sentence_scores, 1.2 * threshold)
+    if len(summary) == 0:
+        summary = sentences[0]
     return summary
 
     
