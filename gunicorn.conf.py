1a94291107162f40499ebe17c851d1c5072049da
RedGhoul <avaneesab5@gmail.com>
2020-04-06 12:18:15 -0400

    workers = 4

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -15,7 +15,7 @@ accesslog = "-"
 # bind = 'unix:%s' % os.path.join(_VAR, 'run/gunicorn.sock')
 bind = '0.0.0.0:8080'
 # workers = 3
-workers = 2
+workers = 4
 
 timeout = 3 * 60  # 3 minutes
 # keepalive = 24 * 60 * 60  # 1 day
